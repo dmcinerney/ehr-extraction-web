b5c25594303baf82223f3467f14f93c601c8ca62
Jered McInerney <mcinerney.de@husky.neu.edu>
2020-03-09 12:06:35 -0400

    added temporary convert_annotations  script

---
+++ convert_annotations.sh
@@ -0,0 +1,8 @@
+mv train/borna train/borna_annotations
+mv train/anne-sophie train/anne-sophie_annotations
+mv validation/anne-sophie validation/anne-sophie_annotations
+mv validation/borna validation/borna_annotations
+mv test/anne-sophie test/anne-sophie_annotations
+mv test/borna test/borna_annotations
+mv val_overlap/anne-sophie val_overlap/anne-sophie_annotations
+mv val_overlap/borna val_overlap/borna_annotations
